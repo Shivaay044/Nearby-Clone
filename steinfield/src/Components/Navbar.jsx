b4c122a21e9305900ecdd933a0af4c9f6d3abd4c
unknown <guptaahimanshi70@gmail.com>
2023-02-24 22:11:33 +0530

    single product

--- steinfield/src/Components/Navbar.jsx
+++ steinfield/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from "react-bootstrap/Image";
 
--- steinfield/src/Pages/AllRoutes.jsx
+++ steinfield/src/Pages/AllRoutes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom"
-
+import SingleProduct from "./SingleProduct/SingleProduct"
 function AllRoutes() {
   return (
     <Routes>
         <Route path={"/"}  element={<h1>Hello World</h1>}/ >
         <Route path={"/product"}  element={<h1>Hello Product</h1>}/ >
-        <Route path={"/product/:id"}  element={<h1>Hello World</h1>}/ >
+        <Route path={"/product/:id"}  element={<SingleProduct/>}/ >
         <Route path={"/cart"}  element={<h1>Hello Cart</h1>}/ >
         <Route path={"/checkout"}  element={<h1>Hello World</h1>}/ >
         <Route path={"/admin"}  element={<h1>Hello World</h1>}/ >
