ea995b88bd27aa0982779f5dd8cfec5782fd658c
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-02-24 20:22:55 +0530

    update day-3

--- steinfield/src/App.js
+++ steinfield/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import AllRoutes from './Pages/AllRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavScrollExample from './Components/Navbar';
+import { Crousel } from './Pages/Home/Crousel';
 
 function App() {
   return (
     <div className="App">
      <AllRoutes/>
      <NavScrollExample/>
+     {/* <Crousel/> */}
     </div>
   );
 }
