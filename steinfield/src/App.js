2b7bf63ad474f4f5094e3009929cad0d37508117
Manishg <manscans16@gmail.com>
2023-02-26 10:31:32 +0530

    day-5

--- steinfield/src/App.js
+++ steinfield/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import { AdminPage } from './Pages/AdminPage';
+import Checkout from './Pages/Checkout/Checkout';
 
 function App() {
   return (
     <div className="App">
      <AdminPage/>
+     {/* <Checkout/> */}
     </div>
   );
 }
